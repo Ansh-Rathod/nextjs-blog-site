e393594202948a4a5510e20c064d965d726a6a62
Ansh rathod <67627096+Ansh-Rathod@users.noreply.github.com>
2021-09-27 07:19:29 -0700

    Fixing post

--- Components/Post.js
+++ Components/Post.js
@@ -2,18 +2,17 @@ import Link from "next/link";
 export default function Post({ post }) {
   return (
     <div className="blog-container">
-      <p className="chip">{post.frontmatter.tags}</p>
       <br />
       <img
         src={post.frontmatter.cover_image}
         alt=""
         className="materialboxed img"
       />
-      <div style={{ height: "16px" }}></div>
+      <div style={{ height: "20px" }}></div>
 
       <p className="date"> {post.frontmatter.date}</p>
 
-      <div style={{ height: "10px" }}></div>
+      <div style={{ height: "20px" }}></div>
 
       <h4>{post.frontmatter.title}</h4>
       <div style={{ height: "10px" }}></div>
